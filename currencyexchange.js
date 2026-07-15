0551fdc4a91dfc57184936260219a72aa11232f1
Atif Memon <atifmemon05@icloud.com>
2026-07-15 14:28:22 -0600

    Created simple built-in exchange rate with 3 types and prompts with user input for amounts

--- currencyexchange.js
+++ currencyexchange.js
@@ -2,4 +2,19 @@ const prompt = require('prompt-sync')();
 
 let dollar = prompt("Enter USD Amount: ")
 
-console.log(dollar + " dollars")
+let conv = prompt("Conversion from USD to: 1 = Euro, 2 = Peso, 3 = Rupee: ")
+
+let dol = parseInt(dollar);
+
+if (conv == "1"){
+    console.log(dol * 0.87)
+}
+else if (conv == "2"){
+    console.log(dol * 17.39)
+}
+else if (conv == "3"){
+    console.log(dol * 96.29)
+}
+else{
+    console.log("Invalid type, Try again.")
+}
