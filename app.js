c32f005c05888576bfff7fc56a9ff8004caf4cac
Atif Memon <atifmemon05@icloud.com>
2026-07-17 21:26:40 -0600

    Made minor updates to further improve GUI

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
 // Locate elements in the DOM (Document Object Model)
 const actionButton = document.getElementById('action-btn');
-const statusText = document.getElementById('status-text');
 
 // Track the state of the GUI application
 let clickCount = 0;
 
 // Listen for a click event on the button
 actionButton.addEventListener('click', () => {
-    clickCount++;
-    statusText.innerText = `Success! The GUI button has been clicked ${clickCount} times.`;
-    statusText.style.color = '#28a745'; // Change text to green
+    
 });
