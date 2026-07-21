6124ba3e291f9b00340dfede662e2f7cd9832bc0
Atif Memon <atifmemon05@icloud.com>
2026-07-20 20:48:32 -0600

    made slight updates, added server file for cleaner API requests, readability, and over smoothness of the program

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
+const exchange = require("./exchLogic.js");
+
 // Locate elements in the DOM (Document Object Model)
 const actionButton = document.getElementById('action-btn');
+const resultTxt = document.getElementById('result-text');
 const convFrom = document.getElementById('ConvFrom');
 const convTo = document.getElementById('ConvTo');
-const amount = document.getElementById('currency');
+const currency = document.getElementById('currency');
 
 
 
 // Listen for a click event on the button
 actionButton.addEventListener('click', () => {
+
+    try{
+        const amount = parseFloat(currency.value);
+        const from = convFrom.trim().toUpperCase();
+        const to = convTo.trim().toUpperCase();
+
+        const result = exchange.convertCurrency(amount, from, to);
+
+        resultTxt.textContent = amount + " " + from + " = " + result + " " + to;
+    } catch (error){
+        resultTxt.textContent = `Error: ${error.message}`;
+    }
     
 });
